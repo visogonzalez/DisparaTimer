9448020663eacb55316ea1ee7c9cebae5703dc74
visogonzalez <visogonzalez@gmail.com>
2024-08-25 15:15:37 +0200

    -Creada carpeta Raspberry par poner el archivo.

---
+++ Raspberry/main.py
@@ -0,0 +1,40 @@
+import network
+import time
+import socket
+from machine import Pin
+
+led=Pin(16,Pin.OUT)
+led2= Pin("LED", Pin.OUT)
+led2.on()
+
+# if you do not see the network you may have to power cycle
+# unplug your pico w for 10 seconds and plug it in again
+def ap_mode(ssid, password):
+    """
+        Description: This is a function to activate AP mode
+
+        Parameters:
+
+        ssid[str]: The name of your internet connection
+        password[str]: Password for your internet connection
+
+        Returns: Nada
+    """
+    # Just making our internet connection
+    ap = network.WLAN(network.AP_IF)
+    ap.config(essid=ssid, password=password, pm = 0x111022)
+    ap.active(True)
+
+    while ap.active() == False:
+        pass
+    print('AP Mode Is Active, You can Now Connect')
+    print('IP Address To Connect to:: ' + ap.ifconfig()[0])
+
+    s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s2.bind(('', 5005))
+
+    while True:
+      data, addre = s2.recvfrom(1024)
+      led.toggle()
+
+ap_mode('DISPARA','DISPARAFILM')
