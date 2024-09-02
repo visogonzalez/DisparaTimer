cd1d0c725441a6e6b11bcaec2858dd6cfc345ef3
visogonzalez <visogonzalez@gmail.com>
2024-09-02 17:55:12 +0200

    -Comentarios. -Apk generada.

--- app/src/main/java/com/ebookfrenzy/disparafilmtimer/MainActivity.java
+++ app/src/main/java/com/ebookfrenzy/disparafilmtimer/MainActivity.java
@@ -68,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Full screen without navigation buttons
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -76,9 +77,9 @@ public class MainActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-
-
         EdgeToEdge.enable(this);
+
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
 
         View view = binding.getRoot();
@@ -86,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
 
         WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
         WindowInsetsControllerCompat windowInsetsController = new WindowInsetsControllerCompat(getWindow(), getWindow().getDecorView());
+
         // Hide system bars
         windowInsetsController.hide(WindowInsetsCompat.Type.systemBars());
 
@@ -117,7 +119,7 @@ public class MainActivity extends AppCompatActivity {
         binding.st9Ac.setOnLongClickListener(v -> acTimer(9));
         binding.st10Ac.setOnLongClickListener(v -> acTimer(10));
 
-        //Reescalado de tamaño de letra campos
+        //Tex size and layout rescaling
         DisplayMetrics metrics = getResources().getDisplayMetrics();
         double factor=Math.sqrt(Math.pow(metrics.heightPixels/ metrics.ydpi,2)+Math.pow(metrics.widthPixels/metrics.xdpi,2))/5.742;
 
@@ -257,6 +259,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
 
+        //ndelay cursor visible on input
         view.getViewTreeObserver().addOnGlobalLayoutListener(() -> {
             Rect r = new Rect();
             view.getWindowVisibleDisplayFrame(r);
